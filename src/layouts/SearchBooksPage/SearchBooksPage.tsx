3d0604f022609df293a8c95db0337deb5e009c85
Tú Trần <nhoxcool2002@gmail.com>
2024-04-19 16:17:53 +0700

    SearchPage

---
+++ src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -0,0 +1,132 @@
+import { useEffect, useState } from "react";
+import BookModel from "../../models/Bookmodel";
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
+import { SearchBook } from "./components/SearchBook";
+
+export const SearchBooksPage = () => {
+  const [books, setBooks] = useState<BookModel[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const bareUrl: string = "http://localhost:8080/api/books";
+
+      const url: string = `${bareUrl}?page=0&size=9`;
+
+      const reponse = await fetch(url);
+
+      if (!reponse.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const responseJson = await reponse.json();
+
+      const responseData = responseJson._embedded.books;
+
+      const loadedBook: BookModel[] = [];
+
+      for (const key in responseData) {
+        loadedBook.push({
+          id: responseData[key].id,
+          title: responseData[key].title,
+          author: responseData[key].author,
+          description: responseData[key].description,
+          copies: responseData[key].copies,
+          copiesAvailable: responseData[key].copiesAvailable,
+          category: responseData[key].category,
+          img: responseData[key].img,
+        });
+      }
+
+      setBooks(loadedBook);
+      setIsLoading(false);
+    };
+    fetchBooks().catch((error: any) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <SpinnerLoading />;
+  }
+
+  if (httpError) {
+    return (
+      <div className="container m-5">
+        <p>{httpError}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div className="container">
+        <div className="row mt-5">
+          <div className="col-6">
+            <div className="d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success">Search</button>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="dropdown">
+              <button
+                className="btn btn-secondary dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Category
+              </button>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="dropdownMenuButton1"
+              >
+                <li>
+                  <a className="dropdown-item" href="#">
+                    All
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Front End
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Back End
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Data
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    DevOps
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="mt-3">
+            <h5>Number of results: (22)</h5>
+          </div>
+          <p>1 to 5 of 22 items:</p>
+          {books.map((book) => (
+            <SearchBook book={book} key={book.id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
